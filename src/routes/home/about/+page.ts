b4d92569eff46e17f8b07e148eda33fe72802dad
mattcroat <mattcroat.dev@gmail.com>
2022-09-04 21:30:56 +0200

    chore: Replace hydrate with csr

--- src/routes/home/about/+page.ts
+++ src/routes/home/about/+page.ts
@@ -1,7 +1,7 @@
 import { dev } from '$app/environment'
 
 // load JavaScript in development for hot module replacement
-export const hydrate = dev
+export const csr = dev
 
 // prerender static page in production
 export const prerender = true
